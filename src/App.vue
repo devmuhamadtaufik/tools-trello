d14c265ebaac1ee8cc909049e55d97255d83c07d
Muhamad Taufik <devmuhamadtaufik@gmail.com>
2024-03-12 10:08:17 +0700

    add Eslint

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 </script>
 
 <template>
-<div class="bg-red-300">
-  <button class="btn btn-primary">Hello World!</button>
-</div>
+  <div class="bg-red-300">
+    <button
+      class="btn btn-primary"
+    >
+      Hello World!
+    </button>
+  </div>
 </template>
-
-<style scoped>
-</style>
